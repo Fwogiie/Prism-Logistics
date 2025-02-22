a30d67c18ae60433d58c7e066cc6a6a0c2af9cdf
Naomi <geennaamacount@gmail.com>
2025-02-22 23:16:46 +0100

    TH downloads command

--- PrismCore/PrismCommands/truckershubdownload.py
+++ PrismCore/PrismCommands/truckershubdownload.py
@@ -1,5 +1,5 @@
 from PrismBot import bot
 
-@bot.command(name="thddownload")
+@bot.command(name="jobtracker")
 async def truckershub_download(ctx):
     await ctx.send("You can download the TruckersHub tracker [here](https://truckershub.in/download)")
