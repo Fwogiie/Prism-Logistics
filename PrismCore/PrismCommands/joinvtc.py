9f39d98487fd6d9650f59d24eeb9c9940d502d57
Naomi <geennaamacount@gmail.com>
2025-02-24 00:35:25 +0100

    commands embeds

--- PrismCore/PrismCommands/joinvtc.py
+++ PrismCore/PrismCommands/joinvtc.py
@@ -1,5 +1,9 @@
 from PrismBot import bot
+import nextcord
 
 @bot.command(name="join")
 async def join_vtc(ctx):
-    await ctx.send("Are you interested in joining our VTC?\n\nThen apply on the TruckersMP website\nAnd then you can also apply in <#1342827168786288650>")
+    embed = nextcord.Embed(title="Are you interested in joining our VTC?",
+                           description="Then apply on the [TruckersMP website](https://truckersmp.com/vtc/78375/)\nThen apply in <#1342827168786288650>",
+                           color=nextcord.Color.purple())
+    await ctx.send(embed=embed)
--- PrismCore/PrismCommands/truckershubdownload.py
+++ PrismCore/PrismCommands/truckershubdownload.py
@@ -1,5 +1,9 @@
 from PrismBot import bot
+import nextcord
 
 @bot.command(name="jobtracker")
 async def truckershub_download(ctx):
-    await ctx.send("You can download the TruckersHub tracker [here](https://truckershub.in/download)")
+    embed = nextcord.Embed(title="TruckersHub job tracker",
+                           description="You can download the TruckersHub tracker [here](https://truckershub.in/download)",
+                           color=nextcord.Color.purple())
+    await ctx.send(embed=embed)
