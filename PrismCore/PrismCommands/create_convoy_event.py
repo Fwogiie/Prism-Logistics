ccd36e62357eace223b7f7ce53d797fd373efe5a
Naomi <geennaamacount@gmail.com>
2025-04-22 10:15:42 +0200

    Now with convoys!

--- PrismCore/PrismCommands/create_convoy_event.py
+++ PrismCore/PrismCommands/create_convoy_event.py
@@ -12,8 +12,8 @@ async def _create(ctx):
     # This does not need any logic as this is meant for subcommands.
     pass
 
-@_create.subcommand(name="convoy", description="KEEP IN MIND, TIME HAS TO BE UNIX TIMESTAMP, AND DLCS SHOULD BE SEPERATED WITH '-'")
-async def _create_convoy(ctx, discord_event_id: str, convoy_url: str, slot: str, slot_img: str=None):
+@_create.subcommand(name="convoy", description="DLCs should be separated with '-'")
+async def _create_convoy(ctx, discord_event_id: str, convoy_url: str, slot: str, slot_img: str, required_dlcs: str="None"):
     await ctx.response.defer()
     discord_event = bot.get_scheduled_event(int(discord_event_id))
     req = requests.get(f"https://api.truckersmp.com/v2/events/{convoy_url.split('/events/')[1]}")
@@ -24,8 +24,9 @@ async def _create_convoy(ctx, discord_event_id: str, convoy_url: str, slot: str,
     await discord_event.edit(name=req["name"],
                              description=f"- TruckersMP event: {convoy_url}\n- Server: {req['server']['name']}\n"
                                          f"# Departure:\n- Location: {req['departure']['location']}\n- City: {req['departure']['city']}\n"
-                                         f"- Our slot: {slot} - Slot image: [image]({slot_img})\n"
-                                         f"# Arriving:\n- Location: {req['arrive']['location']}\n- City: {req['arrive']['city']}",
+                                         f"- Our slot: {slot}\n- Slot image: [image]({slot_img})\n"
+                                         f"# Arriving:\n- Location: {req['arrive']['location']}\n- City: {req['arrive']['city']}\n"
+                                         f"# Requirements:\n- DLCs: {required_dlcs}",
                              start_time=datetime.fromisoformat(req['meetup_at']), end_time=datetime.fromisoformat(req['start_at']),
                              image=requests.get(req['banner']).content)
     await ctx.followup.send("Successfully edited event!")
