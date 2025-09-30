eb9009636d4c50bdecc60ae793baf3fab7dc149f
Naomi <geennaamacount@gmail.com>
2025-09-30 21:54:17 +0200

    (Side Project) update list corposays.py

--- CampysCore/CampysCommands/corposays.py
+++ CampysCore/CampysCommands/corposays.py
@@ -1,13 +1,11 @@
 import json
 import os
 from random import randint
-
 import requests
-
 from PrismBot import bot
 
 
-jobs = ['Coal', 'Iron', 'Logs', 'Cement', 'Concrete', 'Limestone', 'Fuel', 'Crude Oil', 'Milk', 'Container', 'Toys', 'Cheese', 'Bread', 'Sand', 'Plastics', 'Pickup Trash', 'Empty Gapa Farms']
+jobs = ['Coal', 'Iron', 'Logs', 'Cement', 'Concrete', 'Limestone', 'Fuel', 'Crude Oil', 'Milk', 'Container', 'Toys', 'Cheese', 'Bread', 'Sand', 'Plastics', 'Pickup Trash', 'Empty Gapa Farms', 'Wrecked vehicles']
 jobsdelivery = {"Coal": ["Gwangjin Coal Storage", "Steel Mill Steel Coil Coal Drop", "Steel Mill H-Beam Coal Drop"],
                 "Iron": ["Gwangjin Iron Ore Storage", "Steel Mill Steel Coil Iron Ore Drop", "Steel Mill H-Beam Iron Ore Drop"],
                 "Logs": ["Sangdo Lumbermil Drop Off", "Migeum Log Warehouse (doing a 50 20ft logs contract)", "Migeum Log Warehouse (doing a 20 12ft logs contract)"],
@@ -24,7 +22,8 @@ jobsdelivery = {"Coal": ["Gwangjin Coal Storage", "Steel Mill Steel Coil Coal Dr
                 "Sand": ["Concrete Factory Sand Dump"],
                 "Plastics": ["Toy Factory"],
                 "Pickup Trash": ["Gwangjin", "GangJung", "Jeju"],
-                "Empty Gapa Farms": ["Gangjung Warehouse", "Jeju Warehouse", "Sinchang Warehouse", "Oji Gate Warehouse"]}
+                "Empty Gapa Farms": ["Gangjung Warehouse", "Jeju Warehouse", "Sinchang Warehouse", "Oji Gate Warehouse"],
+                "Wrecked vehicles": ["... Anywhere really xD"]}
 
 @bot.slash_command(name="corpo", guild_ids=[1404474707071209502])
 async def _corpo(ctx):
@@ -33,9 +32,6 @@ async def _corpo(ctx):
 @_corpo.subcommand(name="says", description="Generate a job to do together with corpo members!")
 async def corposays(ctx):
     randomnumber = json.loads(requests.post("https://api.random.org/json-rpc/4/invoke", json={"jsonrpc": "2.0", "method": "generateSignedIntegers", "id": 1, "params": {"apiKey": "86b3220b-4f36-4b6d-8eea-59feec9da224", "n": 1, "min": 1, "max": len(jobs), "replacement": False}}).text)["result"]["random"]["data"][0]
-    print(randomnumber)
     pickedjob = jobs[randomnumber-1]
-    print(pickedjob)
-    print(randint(0, len(pickedjob)-1))
     delivery = jobsdelivery[pickedjob][randint(0, len(jobsdelivery[pickedjob])-1)]
     await ctx.send(f"Corpo wants you to deliver some **{pickedjob}** to **{delivery}**, get to work!")
