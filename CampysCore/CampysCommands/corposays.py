045e7bd51091dd1f5a6185137f9b56181ec341a9
Naomi <geennaamacount@gmail.com>
2025-10-03 18:37:46 +0200

    (Side Project) update list corposays.py

--- CampysCore/CampysCommands/corposays.py
+++ CampysCore/CampysCommands/corposays.py
@@ -1,13 +1,15 @@
 import json
 import os
 from random import randint
+
+import nextcord
 import requests
 import tokens
 from PrismBot import bot
 
 
 jobs = ['Coal', 'Iron', 'Logs', 'Cement', 'Concrete', 'Limestone', 'Fuel', 'Crude Oil', 'Milk', 'Container', 'Toys',
-        'Cheese', 'Bread', 'Sand', 'Plastics', 'Pickup Trash', 'Empty Gapa Farms', 'Wrecked vehicles', 'Quicklime', 'Police love']
+        'Cheese', 'Bread', 'Sand', 'Plastics', 'Pickup Trash', 'Empty Gapa Farms', 'Wrecked vehicles', 'Quicklime', 'Police love', 'Planks']
 
 jobsdelivery = {"Coal": ["Gwangjin Coal Storage", "Steel Mill Steel Coil Coal Drop", "Steel Mill H-Beam Coal Drop"],
                 "Iron": ["Gwangjin Iron Ore Storage", "Steel Mill Steel Coil Iron Ore Drop", "Steel Mill H-Beam Iron Ore Drop"],
@@ -28,15 +30,24 @@ jobsdelivery = {"Coal": ["Gwangjin Coal Storage", "Steel Mill Steel Coil Coal Dr
                 "Empty Gapa Farms": ["Gangjung Warehouse", "Jeju Warehouse", "Sinchang Warehouse", "Oji Gate Warehouse"],
                 "Wrecked vehicles": ["... Anywhere really xD"],
                 "Quicklime": ["Gapa Farms", "Main island Farms"],
-                "Police love": ["Main island towns", "Bottom island towns"]}
+                "Police love": ["Main island towns", "Bottom island towns"],
+                "Planks": ["Gwangjin Construction Site 1", "Oji Drilling", "Gwangjin Construction Site F1"]}
 
 @bot.slash_command(name="corpo", guild_ids=[1404474707071209502])
 async def _corpo(ctx):
     pass
 
 @_corpo.subcommand(name="says", description="Generate a job to do together with corpo members!")
-async def corposays(ctx):
+async def _corposays(ctx):
     randomnumber = json.loads(requests.post("https://api.random.org/json-rpc/4/invoke", json={"jsonrpc": "2.0", "method": "generateSignedIntegers", "id": 1, "params": {"apiKey": tokens.randokey, "n": 1, "min": 1, "max": len(jobs), "replacement": False}}).text)["result"]["random"]["data"][0]
     pickedjob = jobs[randomnumber-1]
     delivery = jobsdelivery[pickedjob][randint(0, len(jobsdelivery[pickedjob])-1)]
     await ctx.send(f"Corpo wants you to deliver some **{pickedjob}** to **{delivery}**, get to work!")
+
+@_corposays.subcommand(name="list", description="Show the current jobs that you are able to be given")
+async def _corposaysjoblist(ctx):
+    joblist = "The current jobs present in the list are:\n\n"
+    for x in jobs:
+        joblist += f"**{x}** to **{str(jobsdelivery[x]).strip("[]")}**\n\n"
+    embed = nextcord.Embed(title="Job list", description=joblist, color=nextcord.Color.purple())
+    await ctx.send(embed=embed)
